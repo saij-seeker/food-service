3c7cabcc6923ecffa4106cd0251704a5fe40522d
Saijal <saijal.gulyani@intimetec.com>
2022-12-19 00:55:12 +0530

    made project structure

---
+++ src/main/java/com/example/foodservice/FoodserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodserviceApplication.class, args);
+	}
+
+}
